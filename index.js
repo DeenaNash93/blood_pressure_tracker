b5ad57148f349c184aff2e366d47024e0a7ac889
DeenaNash93 <deenanash93@gmail.com>
2025-02-19 13:36:37 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const port = 7381;
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+const path = require("path");
+app.use(bodyParser.urlencoded({extended: false}));
+
+let db_M = require('./database');
+global.db_pool = db_M.pool;
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, "/views"));
+
+app.use("/CSS1",express.static(path.join(__dirname, "css")));
+app.use("/js",express.static(path.join(__dirname, "js")));
+
+global.htmlspecialchars = require('htmlspecialchars');
+// const { addSlashes, stripSlashes } = require('slashes');
+
+
+
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port http://localhost:${port}`);
+});
